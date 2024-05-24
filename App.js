04256883f457b00fb3789f29acda2f7e31684fb5
xianning liu <xianlinbox@gmail.com>
2024-05-24 08:07:49 -0500

    use prettier to format language

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import { LinearGradient } from 'expo-linear-gradient'
-import { ImageBackground, StyleSheet } from 'react-native'
-import GameStart from './screens/game_start';
+import { LinearGradient } from "expo-linear-gradient";
+import { ImageBackground, StyleSheet } from "react-native";
+import GameStart from "./screens/game_start";
 
 export default function App() {
   return (
-    <LinearGradient style={styles.appContainer} colors={['#4e0329', '#ddb52f']}>
-      <ImageBackground source={require('./assets/images/background.png')}
-        resizeMode='cover'
+    <LinearGradient style={styles.appContainer} colors={["#4e0329", "#ddb52f"]}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
         style={styles.appContainer}
-        imageStyle={{ opacity: 0.15 }}>
+        imageStyle={{ opacity: 0.15 }}
+      >
         <GameStart />
       </ImageBackground>
-    </LinearGradient >
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex: 1
+    flex: 1,
   },
 });
