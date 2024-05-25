068dbafc0a27c9c3675c978a9050a4dac08c4aad
xianning liu <xianlinbox@gmail.com>
2024-05-25 09:52:16 -0500

    add game logic to check input

--- screens/game_start.js
+++ screens/game_start.js
@@ -1,12 +1,33 @@
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, TextInput, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/primary_button";
 function GameStart() {
-  function reset() {}
+  const [enteredNumber, setEnteredNumber] = useState("");
+  function reset() {
+    setEnteredNumber("");
+  }
 
-  function confirm() {}
+  function confirm() {
+    const inputNumber = parseInt(enteredNumber);
+    if (isNaN(inputNumber) || inputNumber <= 0 || inputNumber > 99) {
+      Alert.alert("Invalid Number", "This game only allow number between 1 and 99!", [
+        { title: "Okay", style: "default", onPress: reset },
+      ]);
+    }
+  }
+
+  function textInputHandler(input) {
+    setEnteredNumber(input);
+  }
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.input} maxLength={2} keyboardType="number-pad" />
+      <TextInput
+        style={styles.input}
+        maxLength={2}
+        keyboardType="number-pad"
+        value={enteredNumber}
+        onChangeText={textInputHandler}
+      />
       <View style={styles.buttonContainer}>
         <View style={styles.button}>
           <PrimaryButton title="Reset" onPressHandler={reset} />
