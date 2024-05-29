3b58f174b3693c3ff2088e66f95b313b6ba88507
xianning liu <xianlinbox@gmail.com>
2024-05-29 11:31:18 -0500

    structure game over screen

--- screens/game_over.js
+++ screens/game_over.js
@@ -1,7 +1,33 @@
-import { Text } from "react-native";
-
-function GameOver() {
-  return <Text>Game OVER!</Text>;
+import { Text, View, Image, StyleSheet } from "react-native";
+import { Title } from "../components/title";
+import { PrimaryButton } from "../components/primary_button";
+function GameOver({ startNewGame }) {
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/images/success.png")}></Image>
+      </View>
+      <Text></Text>
+      <PrimaryButton onPressHandler={startNewGame}>Start New Game</PrimaryButton>
+    </View>
+  );
 }
 
 export default GameOver;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {},
+  image: {
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+    overflow: "hidden",
+  },
+});
