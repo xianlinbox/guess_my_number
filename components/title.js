bf163e1e81677c8b406b47b764d9f47ef43916fb
xianning liu <xianlinbox@gmail.com>
2024-05-31 15:16:02 -0500

    use Platform API to behave different on iOS or Android

--- components/title.js
+++ components/title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 
 function Title({ children }) {
   return <Text style={styles.text}>{children}</Text>;
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "white",
     textAlign: "center",
-    borderWidth: 2,
+    borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: "white",
     padding: 12,
   },
